5f48daf11d7e62375d4b3fb9a5891851fe44cd3a
Sophie Shepherd <sophshep@gmail.com>
2015-05-26 20:40:25 -0500

    Add notches to make Lake Height an infographic

--- js/script.js
+++ js/script.js
@@ -4,14 +4,16 @@ $.getJSON('http://anyorigin.com/dev/get?url=http%3A//hydromet.lcra.org/riverrepo
   var cleanHTML = data.contents.replace(/<img.*?\/>/g, '');
   var html = $(cleanHTML);
   var maxLakeLevel = 681;
+  var maxLakeMarker = 730;
   // var xpath = '#GridView1/tr[3]/td[3];
   var lakeHeight = parseFloat(html.find('#GridView1 tr:nth-of-type(3) td:nth-of-type(3)').text());
 
-  if (lakeHeight >= maxLakeLevel) {
+  if (lakeHeight >= 681) {
     document.querySelector('#status').textContent = 'Yup';
   } else {
     var waterLevel = document.querySelector('#water-level');
-    waterLevel.style.height = (lakeHeight / maxLakeLevel) * 100 + '%';
+    // waterLevel.style.height = (lakeHeight / maxLakeLevel) * 100 + '%';
+    waterLevel.style.height = (lakeHeight / maxLakeMarker) * 100 + '%';
     waterLevel.style.maxHeight = '9999px';
     document.querySelector('#remainder').textContent = Math.round(maxLakeLevel - lakeHeight) + ' More Feet To Go'
   }
